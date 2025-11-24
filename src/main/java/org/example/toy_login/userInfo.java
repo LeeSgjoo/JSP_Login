8b3faf76a0f61c2d4ebb409b1bfdf9bce981dbec
joo <joo@jooui-MacBookPro.local>
2025-11-24 21:33:05 +0900

    getter 수정

--- src/main/java/org/example/toy_login/userInfo.java
+++ src/main/java/org/example/toy_login/userInfo.java
@@ -5,18 +5,24 @@ public class userInfo{
     private String password;
 
     public userInfo(String name) {
-        this.name = name;
+        this.name = "???";
+        this.password = "???";
     }
+    //<jsp:useBean> 태그는 지정된 id에 해당하는 객체가 scope에 없을 때,
+    // 기본적으로 인자 없는(no-argument) 생성자를 호출하여 객체를 생성합니다.
+    // => 파라미터 없는 setter 생성자도 만들어야 한다.
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+    public void setName() {} //비어있더라도 jsp:setProperty에서 폼에서 받아온대로 잘 맵핑시켜준다.
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
+    public void setPassword(){}
 }
--- src/main/webapp/login_ok.jsp
+++ src/main/webapp/login_ok.jsp
@@ -1,12 +1,12 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-
+<%--  만일 한글이 깨진다면 사용 => <%request.setCharacterEncoding("UTF-8");%>    --%>
 <!DOCTYPE html>
 <html>
 <head>
     <title>JSP - Hello World</title>
 </head>
 <body>
-    <jsp:useBean id = "user" class = "org.example.toy_login/userInfo" scope = "session"/>
+    <jsp:useBean id = "user" class = "org.example.toy_login.userInfo" scope = "session"/>
 <%--    // 현재 페이지에서만 쓸 것이라면 scope를 지정하지 않아도 되지만 여러 페이지에 걸쳐 전체에서 사용하기 위해 session으로 scope를 지정해줬다.--%>
     <jsp:setProperty property = "*" name = "user"/>
 <%--    // form에서 input으로 가져온 name에 일치하는 객체로 넘어온 정보들이 모두 자동으로 세팅된다.--%>
