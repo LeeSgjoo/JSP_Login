3d7f769ac3647ce927da4a1153d8398f49b47692
joo <joo@jooui-MacBookPro.local>
2025-11-24 21:35:53 +0900

    feat:login 생성자 수정

--- src/main/java/org/example/toy_login/userInfo.java
+++ src/main/java/org/example/toy_login/userInfo.java
@@ -10,19 +10,19 @@ public class userInfo{
     }
     //<jsp:useBean> 태그는 지정된 id에 해당하는 객체가 scope에 없을 때,
     // 기본적으로 인자 없는(no-argument) 생성자를 호출하여 객체를 생성합니다.
-    // => 파라미터 없는 setter 생성자도 만들어야 한다.
+    // => 파라미터 없는 기본 생성자도 만들어야 한다.
+    // 그래야 jsp:setProperty에서 form으로 받은 데이터를 맴버변수에 맵핑시켜준다.
+    public userInfo(){};
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public void setName() {} //비어있더라도 jsp:setProperty에서 폼에서 받아온대로 잘 맵핑시켜준다.
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
-    public void setPassword(){}
 }
